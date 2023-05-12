fd4b4b01eccdb41a48265d2c42897e2fb889a180
stevim <i.m.steveim@gmail.com>
2023-05-12 17:23:43 -0400

    write intial service functions

--- src/pages/DayDetails/DayDetails.jsx
+++ src/pages/DayDetails/DayDetails.jsx
@@ -1,6 +1,16 @@
+import ExerciseCard from '../../components/ExerciseCard/ExerciseCard'
+import MealCard from '../../components/MealCard/MealCard'
+import NoteCard from '../../components/NoteCard/NoteCard'
+import SleepCard from '../../components/SleepCard/SleepCard'
+
 const DayDetails = () => {
   return (
-
+    <>
+      <SleepCard />
+      <MealCard />
+      <ExerciseCard />
+      <NoteCard />
+    </>
   )
 }
 
