4e6cc82d1d0afd5dacdc4c47b36c9f1df09037eb
Eugene <eugenefung79@gmail.com>
2023-05-13 10:05:48 -0400

    add route for daylist index

--- src/components/DayCard/DayCard.jsx
+++ src/components/DayCard/DayCard.jsx
@@ -1,6 +1,20 @@
-const DayCard = () => {
-  return (
+// npm modules
+import { Link } from "react-router-dom"
 
+
+const DayCard = ({ day }) => {
+  return (
+    <Link to={`/days/${day._id}`} state={day}>
+      <article>
+        <header>
+          <span>
+            this is the day index
+            {/* <h1>{ day.date }</h1> */}
+           {/* This is the text inside the card, what should go here besides date? */}
+          </span>
+        </header>
+      </article>
+    </Link>
   )
 }
 
