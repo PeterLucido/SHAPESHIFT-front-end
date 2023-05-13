d631587043f9fa9bd20919ed71e71eb897a866f9
Peter Lucido <petermlucido@gmail.com>
2023-05-13 12:55:06 -0500

    new day component

--- src/pages/NewDay/NewDay.jsx
+++ src/pages/NewDay/NewDay.jsx
@@ -1,6 +1,39 @@
-const NewDay = () => {
+import { useState } from "react"
+
+
+
+const NewDay = (props) => {
+  const [dayFormData, setDayFormData]= useState({
+    date: new Date().toISOString().slice(0,10),
+    rating: 1,
+  })
+
+  const handleDayChange = (evt) => {
+    setDayFormData({...dayFormData, [evt.target.name]: evt.target.value})
+  }
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    props.handleAddDay(dayFormData)
+  }
+
+
   return (
-    <h1>NewDay Page</h1>
+    <main>
+      <h1>Create Day</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='date-input'>Date</label>
+        <input
+          required
+          type='date'
+          name='date'
+          id='date-input'
+          value={dayFormData.date}
+          onChange={handleDayChange}
+        />
+        <button type='submit'>SUBMIT</button>
+      </form>
+    </main>
   )
 }
 
