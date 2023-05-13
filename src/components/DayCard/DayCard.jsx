d6713ec7a810c675e385ef4a8d3812a5467fbbbe
stevim <i.m.steveim@gmail.com>
2023-05-13 12:29:40 -0400

    format dayCard

--- src/components/DayCard/DayCard.jsx
+++ src/components/DayCard/DayCard.jsx
@@ -4,13 +4,23 @@ import { Link } from "react-router-dom"
 
 
 const DayCard = ({ day }) => {
+
+  
   return (
-    <Link to={`/days/${day._id}`} state={day}>
-        <header>
-            <h1>{ day.date }</h1>
-            <h2>{day.rating}</h2>
-        </header>
-    </Link>
+    <>
+      <div>
+        <Link to={`/days/${day._id}`}>
+          <h4>{day.date.slice(0,10)}</h4>
+        </Link>
+        <h4>Rating: {day.rating}/5</h4>
+      </div>
+    </>
+    // <Link to={`/days/${day._id}`} state={day}>
+    //     <header>
+    //         <h1>{ day.date }</h1>
+    //         <h2>{day.rating}</h2>
+    //     </header>
+    // </Link>
   )
 }
 
