2effb48d3bc0c7e3c6540118b50273131172aeab
stevim <i.m.steveim@gmail.com>
2023-05-15 10:58:19 -0400

    fix quote fetching

--- src/components/QuoteCard/QuoteCard.jsx
+++ src/components/QuoteCard/QuoteCard.jsx
@@ -5,29 +5,24 @@ function QuoteCard() {
   const [quote, setQuote] = useState(null)
 
   useEffect(() => {
-    async function fetchQuote() {
-      try {
-        const response = await fetchQuoteFromAPI()
-        const quoteData = await response.json()
-        const randomIndex = Math.floor(Math.random() * quoteData.length)
-        const randomQuote = quoteData[randomIndex]
-        setQuote(randomQuote)
-      } catch (err) {
-        console.log(err)
-      }
+    const fetchQuote = async () => {
+      const quoteData = await fetchQuoteFromAPI()
+      const randomIndex = Math.floor(Math.random() * quoteData.length)
+      const randomQuote = quoteData[randomIndex]
+      setQuote(randomQuote)
     }
     fetchQuote()
   }, [])
 
   return (
     <div>
-      {quote ? (
+      {!quote ? (
+        <p>Loading...</p>
+      ) : (
         <div>
           <p>{quote.text}</p>
           <p>- {quote.author}</p>
         </div>
-      ) : (
-        <p>Loading...</p>
       )}
     </div>
   );
--- src/services/quoteService.js
+++ src/services/quoteService.js
@@ -2,10 +2,6 @@ const BASE_URL='https://type.fit/api/quotes'
 
 
 export async function fetchQuoteFromAPI() {
-  try {
-    const res = await fetch(BASE_URL)
-    return res.json()
-  } catch (err) {
-    console.log(err)
-  }
+  const res = await fetch(BASE_URL)
+  return res.json()
 }
