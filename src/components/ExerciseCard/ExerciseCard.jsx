be463cda357d5ceb00df0e83065c80112799b126
Dawson Myers <Djmyers2002@gmail.com>
2023-05-16 14:34:20 -0400

    Adds Exercise Card

--- src/components/ExerciseCard/ExerciseCard.jsx
+++ src/components/ExerciseCard/ExerciseCard.jsx
@@ -1,32 +1,93 @@
 import { useState } from "react"
+import { update } from "../../services/dayService"
 
 const ExerciseCard = ({day}) => {
   const [editMode, setEditMode] = useState(false)
+  const [exerciseData, setExerciseData] = useState({
+    typeOfExercise: '',
+    timeSpent: '',
+  })
 
+  const handleChange = (evt) => {
+    const {name, value} = evt.target
+    setExerciseData((prevData) => ({
+      ...prevData, [name]: value
+    }))
+  }
+  
   const handleEdit = () => {
     setEditMode(true)
+    setExerciseData({
+      timeSpent,
+      typeOfExercise,
+    })
+  }
+  
+  const handleSave = async () => {
+    const updatedDay = {...day}
+    updatedDay.exercise[0] = exerciseData
+    try {
+      await update(day)
+      setEditMode(false)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-  const handleSave = () => {
-    setEditMode(false)
-  }
+  const { typeOfExercise, timeSpent } = day.exercise[0]
 
   if (!day ) return <h1>loading</h1>
-  
+
   const editView = (
     <>
-      <h1>Edit Card View</h1>
+      <h1>Edit Exercise</h1>
+      <h2>
+        <label htmlFor="typeOfExercise-input">Exercises: </label>
+        <textarea 
+        name="typeOfExercise" 
+        id="typeOfExercise-input" 
+        value={exerciseData.typeOfExercise} 
+        onChange={handleChange}
+        />
+        <input 
+        type="number" 
+        name="timeSpent"
+        value={exerciseData.timeSpent}
+        onChange={handleChange}
+        />
+      </h2>
       <button onClick={() => handleSave()}>Save</button>
     </>
   )
-  
+
   const saveView = (
     <>
-      <h1>Card Info</h1>
+      <table>
+        <thead>
+          <tr>
+            <th colSpan={2}>Exercise</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Type of Exercise:</td>
+            <td>
+              <ul>
+                {typeOfExercise.split(",").map((item, index) => (
+                  <li key={index}>{item.trim()}</li>
+                ))}
+              </ul>
+            </td>
+          </tr>
+          <tr>
+            <td>Time Spent:</td>
+            <td>{timeSpent}</td>
+          </tr>
+        </tbody>
+      </table>
       <button onClick={() => handleEdit()}>Edit</button>
     </>
   )
-
 
   return (
     <>
